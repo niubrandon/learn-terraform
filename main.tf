12c56ab85b3d68b6bd09449dee38be0fd188f07b
Brandon Niu <niubrandon@gmail.com>
2024-05-11 10:44:18 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  cloud {
+    organization = "niubrandon-org"
+    workspaces {
+      name = "terraform-aws-instance"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# variables.tf
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
